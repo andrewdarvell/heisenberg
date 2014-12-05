4e5875322b9b0f5d3571df94dc41f71c76ddfcd5
andrewdarvell <andrew@darvell.ru>
2014-12-05 17:11:17 +0300

    Test gamedev

--- desktop/src/ru/darvell/heisenberg/desktop/DesktopLauncher.java
+++ desktop/src/ru/darvell/heisenberg/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package ru.darvell.heisenberg.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.darvell.heisenberg.Game;
+import ru.darvell.heisenberg.HeisenbergGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		config.width = 800;
+		config.height = 600;
+		config.title = "Heisenberg\'s Revenge";
+		new LwjglApplication(new HeisenbergGame(), config);
 	}
 }

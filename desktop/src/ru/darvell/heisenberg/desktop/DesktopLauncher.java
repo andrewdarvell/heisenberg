689c22f59e10d1b3a39e856d759ac9e225d1d654
andrewdarvell <andrew@darvell.ru>
2014-12-05 15:14:20 +0300

    Firs commit heisenberg.

---
+++ desktop/src/ru/darvell/heisenberg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.darvell.heisenberg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.darvell.heisenberg.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
